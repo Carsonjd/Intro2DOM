bcb556af0555a75d217cca73e3371f0bfe5b0851
Jonathan Carson <carsonjd@colorado.edu>
2017-10-11 11:53:23 -0600

    final

--- main.js
+++ main.js
@@ -6,12 +6,20 @@ function changePic (){
 
 //Change the headings text color to blue
 //HINT: it's a class!
-
+var headings = document.getElementsByClassName('headings');
+for (var i=0;i<headings.length;i++){
+  headings[i].style.color = 'blue';
+}
 
 //Change the button's background-color to green
-
+var button = document.getElementById('button');
+console.log(button);
+button.style.background = 'green';
 
 //Change the paragraph font to 'Tahona'
-
+var para = document.getElementsByClassName('paragraph')[0];
+para.style.fontFamily = 'Arial';
 
 //Change the entire documents background color to yellow
+var doc = document.getElementsByTagName('html')[0];
+doc.style.backgroundColor = 'yellow';
